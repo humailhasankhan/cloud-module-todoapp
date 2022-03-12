979e28b0291cb07e15e3c66f68f2a35c01f74f92
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-12 01:43:22 +0000

    add footer and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { Fragment } from "react";
 import "./App.css";
-
-//components
-
 import InputTodo from "./components/InputTodo";
 import ListTodos from "./components/ListTodos";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +10,7 @@ function App() {
       <div className="container">
         <InputTodo />
         <ListTodos />
+        <Footer />
       </div>
     </Fragment>
   );
